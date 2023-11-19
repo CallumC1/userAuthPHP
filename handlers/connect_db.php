e6a11a0f407b954da881dc5b34781661a06c2972
CallumC <51034701+CallumC1@users.noreply.github.com>
2023-11-19 23:54:18 +0000

    more database connection changes

--- handlers/connect_db.php
+++ handlers/connect_db.php
@@ -7,7 +7,10 @@ define('DB_NAME', 'phpuserauth');
 
 function connect_to_database() {
 
-    $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    /* @ is used to suppress warnings  -> https://www.php.net/manual/en/mysqli.connect-error.php */
+    mysqli_report(MYSQLI_REPORT_OFF);
+
+    $conn = @new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
     
     if ($conn->connect_error) {
         // Log the error instead of displaying it to the user to prevent credential leaks.
