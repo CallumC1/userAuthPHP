474f5ca7bc9154d656f0005d54d016ae31cb016f
CallumC <51034701+CallumC1@users.noreply.github.com>
2023-11-19 23:42:48 +0000

    Fixed potential security issue with with db

--- handlers/connect_db.php
+++ handlers/connect_db.php
@@ -1,16 +1,19 @@
 <?php
-function connect_to_database() {
-    $hostname = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "phpuserauth";
 
-    $conn = new mysqli($hostname, $username, $password, $database);
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'phpuserauth');
+
+function connect_to_database() {
+
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
     
     if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+        // Log the error instead of displaying it to the user to prevent credential leaks.
+        error_log("Connection failed: " . $conn->connect_error);
+        die("Internal server error. Please try again later.");
     }
 
-    // Return database connection.
     return $conn;
 };
